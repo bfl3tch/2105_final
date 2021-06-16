92dd542110dc6c3d6acdc37ad13e9ba609e889de
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-16 11:30:03 -0400

    Craete initial spec tests for trainyard

---
+++ spec/train_yard_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/car'
+require './lib/train'
+require './lib/train_yard'
+
+RSpec.describe TrainYard do
+  describe 'instantiation' do
+    it 'creates a new train' do
+      train_yard = TrainYard.new({location: 'Brighton'})
+
+      expect(train_yard).to be_an_instance_of(TrainYard)
+    end
+
+    it 'has readable attributes' do
+      train_yard = TrainYard.new({location: 'Brighton'})
+
+      expect(train_yard.location).to eq('Brighton')
+    end
+  end
+end
