eaea7039e36be65b87dc8534f7579c1f67ca94f0
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-16 11:19:34 -0400

    Create methods to pass spec tests for train

--- lib/train.rb
+++ lib/train.rb
@@ -1,9 +1,17 @@
 class Train
   attr_reader :name, :type, :cargo
-  
+
   def initialize(info)
     @name = info[:name]
     @type = info[:type]
-    @cargo = Hash.new
+    @cargo = Hash.new(0)
+  end
+
+  def count_cars(car)
+    @cargo[car]
+  end
+
+  def add_cars(car, num)
+    @cargo[car] += num
   end
 end
