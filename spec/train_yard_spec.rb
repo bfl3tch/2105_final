6da2dc39d730ceb00791e7f34206e335f1b95e38
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-16 11:57:49 -0400

    Create spec test for sorted cargo list

--- spec/train_yard_spec.rb
+++ spec/train_yard_spec.rb
@@ -102,6 +102,32 @@ RSpec.describe TrainYard do
       expected = [train1, train2, train3]
 
       expect(train_yard.trains_containing(car1)).to eq(expected)
+      expect(train_yard.trains_containing(car2)).to eq([train1])
+    end
+
+    it 'can create a sorted cargo list' do
+      train_yard = TrainYard.new({location: 'Brighton'})
+      car1 = Car.new({type: 'Mail', weight: 5})
+      car2 = Car.new({type: 'Passengers', weight: 1})
+      car3 = Car.new({type: 'Cattle', weight: 3})
+      car4 = Car.new({type: 'Dining', weight: 4})
+      train1 = Train.new({name: 'Thomas', type: 'Tank'})
+      train2 = Train.new({name: 'Emily', type: 'Tender'})
+      train3 = Train.new({name: 'Nia', type: 'Tank'})
+      train4 = Train.new({name: 'Gordon', type: 'Express'})
+      train1.add_cars(car1, 5)
+      train2.add_cars(car1, 3)
+      train2.add_cars(car4, 4)
+      train3.add_cars(car1, 4)
+      train4.add_cars(car4, 5)
+      train4.add_cars(car3, 10)
+      train_yard.add_train(train1)
+      train_yard.add_train(train2)
+      train_yard.add_train(train3)
+      train_yard.add_train(train4)
+      expected = ['Cattle', 'Dining', 'Mail']
+
+      expect(train_yard.sorted_cargo_list).to eq(expected)
     end
   end
 end
