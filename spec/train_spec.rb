a5fd40be9c7f4e46452060c62832f8991f34f348
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-16 11:19:24 -0400

    Create all spec tests through it1 for train

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -18,4 +18,35 @@ RSpec.describe Train do
       expect(train1.cargo).to eq({})
     end
   end
+
+  describe 'it has a method that' do
+    it 'can count the cars in the train' do
+      train1 = Train.new({name: 'Thomas', type: 'Tank'})
+      car1 = Car.new({type: 'Mail', weight: 5})
+      car2 = Car.new({type: 'Passengers', weight: 1})
+
+      expect(train1.count_cars(car1)).to eq(0)
+    end
+
+    it 'can add cars to the cargo count' do
+      train1 = Train.new({name: 'Thomas', type: 'Tank'})
+      car1 = Car.new({type: 'Mail', weight: 5})
+      car2 = Car.new({type: 'Passengers', weight: 1})
+      expect(train1.count_cars(car1)).to eq(0)
+
+      train1.add_cars(car1, 2)
+
+      expect(train1.count_cars(car1)).to eq(2)
+      expect(train1.cargo).to eq({car1 => 2})
+
+      train1.add_cars(car1, 3)
+
+      expect(train1.count_cars(car1)).to eq(5)
+      expect(train1.cargo).to eq({car1 => 5})
+
+      train1.add_cars(car2, 10)
+
+      expect(train1.cargo).to eq({car1 => 5, car2 => 10})
+    end
+  end
 end
